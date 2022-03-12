d053bd957d736c37e012d947c4152ed1c508ff6b
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-03-12 11:58:26 -0500

    change medium editable form to create post

--- components/common/EditableContent/index.jsx
+++ components/common/EditableContent/index.jsx
@@ -5,32 +5,63 @@ import { useForm } from "../../../hooks/useForm";
 const EditableContent = () => {
 
   const [values, handleChange] = useForm({
-    label: ""
+    label: "gaa"
   });
+
+  const [visibleTextarea, setVisibleTextarea] = useState(false);
 
   const { label } = values; 
 
+  const onMouseEnterHeading = ()=>{
+    setVisibleTextarea(true);
+  }
+
+  const onMouseLeaveHeading = ()=>{}
+
+  const onMouseEnterTextarea = ()=>{ }
+
+  const onMouseLeaveTextarea = ()=>{
+  }
+
+  const onMouseBlurTextarea = ()=>{
+    setVisibleTextarea(false);
+}
 
   return (
     <>
-        <Heading as="h3" size="xl" mt="10px" fontFamily={'Georgia,Cambria,"Times New Roman",Times,serif'} fontWeight={"normal"} >
+        {
+            visibleTextarea && (
+                <textarea
+                        onMouseEnter={onMouseEnterTextarea}
+                        onMouseLeave={onMouseLeaveTextarea}
+                        onBlur={onMouseBlurTextarea}
+                        style={{
+                            border: "none !important",
+                            resize: "none",
+                            overflow: "hidden",
+                            outline: "none",
+                            fontFamily: 'Georgia,Cambria,"Times New Roman",Times,serif',
+                            mt: "10px",
+                            fontSize: "2.25rem",
+                            width: "100%",
+                            lineHeight: "1.2",
+                        }}
+                        onChange={handleChange}
+                        name="label"
+                        value={label}
+                        />
+            )
+        }
+        {
+            !visibleTextarea && (
+                <Heading as="h3" size="xl" mt="10px" 
+            onMouseEnter={onMouseEnterHeading}
+            onMouseLeave={onMouseLeaveHeading}
+            fontFamily={'Georgia,Cambria,"Times New Roman",Times,serif'} fontWeight={"normal"} >
             {label}
         </Heading>
-        <textarea
-            style={{
-                border: "none !important",
-                resize: "none",
-                overflow: "hidden",
-                outline: "none",
-                fontFamily: 'Georgia,Cambria,"Times New Roman",Times,serif',
-                mt: "10px",
-                fontSize: "2.25rem",
-                width: "100%",
-            }}
-            onChange={handleChange}
-            name="label"
-            value={label}
-            />
+            )
+        }
     </>
   )
 }
