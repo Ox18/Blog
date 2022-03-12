8c89d8cf4a73bdff7eb5942d6f1cb31d42d58612
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-03-12 15:24:48 -0500

    definidos titulos

--- components/common/EditableContent/index.jsx
+++ components/common/EditableContent/index.jsx
@@ -2,29 +2,74 @@ import React, { useState, useRef } from 'react'
 import { Box } from '@chakra-ui/react'
 import Placeholder from './Placeholder'
 
+const defaultStyleText = {
+    fontFamily: 'Georgia,Cambria,"Times New Roman",Times,serif',
+    lineHeight: "1.2",
+    fontWeight: "400",
+}
+
 const styleText = {
     title: {
-        fontSize: "2.9em",
-        fontFamily: 'Georgia,Cambria,"Times New Roman",Times,serif',
-        lineHeight: "1.2",
+        ...defaultStyleText,
+        lineHeight: "1.25",
+        letterSpacing: "0",
+        fontSize: "42px",
+        paddingTop: "16px"
+    },
+    subtitle: {
+        ...defaultStyleText,
+        color: "rgba(0,0,0,.54)",
+        fontSize: "28px",
+        letterSpacing: "-.012em",
+        lineHeight: "1.22",
+        fontWeight: "300",
+        marginTop: "5px",
+        marginBottom: "22px"
+    },
+    h1: {
+        ...defaultStyleText,
+        fontWeight: "700",
+        fontSize: "34px",
+        lineHeight: "1.5",
+        letterSpacing: "-.015em",
+        marginTop: "45px"
+    },
+    h2: {
+        ...defaultStyleText,
+        fontWeight: "700",
+        fontSize: "30px",
+        lineHeight: "1.5",
+        letterSpacing: "-.01em",
+        marginTop: "35px"
+    },
+    h3: {
+        ...defaultStyleText,
+        fontSize: "26px",
+        fontWeight: "700",
+        lineHeight: "1.22",
+        letterSpacing: "-.012em",
+        marginTop: "31px"
     },
     p: {
-        fontSize: "21px",
-        fontFamily: 'Georgia,Cambria,"Times New Roman",Times,serif',
-        lineHeight: "1.2",
+        ...defaultStyleText,
+        marginTop: "6px",
+        lineHeight: "1.58",
+        letterSpacing: "-.003em",
+        fontSize: "21px"
     }
 }
 
 const TYPES = {
-    P: "p",
+    TITLE: "title",
+    SUBTITLE: "subtitle",
     H1: "h1",
     H2: "h2",
     H3: "h3",
-    TITLE: "title",
+    P: "p",
 }
 
 const EditableContent = ({
-    type = TYPES.TITLE,
+    type = TYPES.P,
     placeholder = "Escribe algo",
 }) => {
     const [label, setLabel] = useState("");
--- pages/new-history.js
+++ pages/new-history.js
@@ -7,9 +7,26 @@ export default function NewHistory() {
     <NewHistoryLayout>
         <Box mt={"20px"} pt={"0px"} pb={"5px"}>
             <Box maxW={"740px"} pl={"20px"} margin={"0 auto"} width={"100%"} pr={"20px"}>
+                
                 <EditableContent 
                     type="title"
                     placeholder="Titulo"
+                />
+                <EditableContent 
+                    type="subtitle"
+                    placeholder="Subtitulo"
+                />
+                <EditableContent 
+                    type="h1"
+                    placeholder="H1"
+                />
+                <EditableContent 
+                    type="h2"
+                    placeholder="H2"
+                />
+                <EditableContent 
+                    type="h3"
+                    placeholder="H3"
                 />
                 <EditableContent 
                     type="p"
